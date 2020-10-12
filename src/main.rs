1ab09688a0333d251c6ee18b5e14ccb49e37a33c
NicolenaStiles <nicolena.stiles@gmail.com>
2020-10-12 17:05:58 -0400

    Design considerations in seperate draw io doc.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate termion;
+
+use std::{thread, time};
+
+use termion::color;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout, stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let one_second = time::Duration::from_millis(1000);
+    let mut now = time::Instant::now();
+    let mut then = now + one_second;
+    println!("BEGIN!!");
+
+    loop {
+        thread::sleep(one_second);
+        println!("hemlo");
+    }
 }
