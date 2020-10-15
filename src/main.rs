8d7de2cd282f7883beb28cbd34526a92b2c6e84b
NicolenaStiles <nicolena.stiles@gmail.com>
2020-10-15 16:32:09 -0400

    able to move the snake head in the terminal

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,84 @@ use termion::raw::IntoRawMode;
 use std::io::{Write, stdout, stdin};
 
 fn main() {
+    // current position of snake head
+    let mut current_position : (u16,u16) = (40,12);
     let one_second = time::Duration::from_millis(1000);
     let mut now = time::Instant::now();
     let mut then = now + one_second;
-    println!("BEGIN!!");
 
-    loop {
-        thread::sleep(one_second);
-        println!("hemlo");
+    // input and output declarations
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    // writing the menu
+    write!(stdout,
+           "{}{}{}O",
+           termion::cursor::Goto(current_position.0, current_position.1),
+           termion::clear::All,
+           termion::cursor::Hide)
+            .unwrap();
+
+    stdout.flush().unwrap();
+
+    let stdin = stdin();
+    // thread::sleep(one_second);
+
+    write!(stdout,
+           "{}{}{}O",
+           termion::cursor::Goto(current_position.0,current_position.1),
+           termion::clear::All,
+           termion::cursor::Hide)
+            .unwrap();
+
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => {
+                write!(stdout, "{}{}",termion::clear::All, termion::cursor::Show).unwrap();
+                break;
+            },
+            Key::Up => {
+                current_position.1 -= 1;
+                write!(stdout,
+                        "{}{}{}O",
+                        termion::clear::All,
+                        termion::cursor::Goto(current_position.0,current_position.1),
+                        termion::cursor::Hide)
+                        .unwrap();
+            }
+            Key::Down => {
+                current_position.1 += 1;
+                write!(stdout,
+                        "{}{}{}O",
+                        termion::clear::All,
+                        termion::cursor::Goto(current_position.0,current_position.1),
+                        termion::cursor::Hide)
+                        .unwrap();
+            }
+            Key::Left => {
+                current_position.0 -= 1;
+                write!(stdout,
+                        "{}{}{}O",
+                        termion::clear::All,
+                        termion::cursor::Goto(current_position.0,current_position.1),
+                        termion::cursor::Hide)
+                        .unwrap();
+            }
+            Key::Right => {
+                current_position.0 += 1;
+                write!(stdout,
+                        "{}{}{}O",
+                        termion::clear::All,
+                        termion::cursor::Goto(current_position.0,current_position.1),
+                        termion::cursor::Hide)
+                        .unwrap();
+            }
+            _ => {}
+        }
+        stdout.flush().unwrap();
     }
+
+    stdout.flush().unwrap();
+
 }
